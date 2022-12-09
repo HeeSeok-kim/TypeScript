a6cfda7e68b7d4fa67167c4749bf7a5ac71d765c
kimheeseok <kim1140129@gmail.com>
2022-12-09 18:03:29 +0900

    fix: 원사타입 / 객체 타입 문법 정리

--- src/EverydayTypes.ts
+++ src/EverydayTypes.ts
@@ -1,17 +1,25 @@
 /**
  * 원시타입 문법
  * */
-const numberType: number = 1234; //숫자 타입
-const stringType: string = 'ABCD'; //문자 타입
-const booleanType: boolean = true; //불린 타입
-const undefinedType: undefined = undefined; //undefined 타입
-const nullType: null = null; //null 타입
-const symbolType: symbol = Symbol.for('abc');
+let numberType: number = 1234; //숫자 타입
+let stringType: string = 'ABCD'; //문자 타입
+let booleanType: boolean = true; //불린 타입
+let undefinedType: undefined = undefined; //undefined 타입
+let nullType: null = null; //null 타입
+let symbolType: symbol = Symbol.for('abc');
+
+//고정된 원시값을 넣을 수도 있어요
+const number:5 = 5;
+
 /**
  * 특별한 경우가 아니면 쓰지 말아야 하는 타입
  * **/
 let anyType:any = "123123"
 anyType = "ABC";
+
+/**
+ *
+ * */
 
 /**
  * 함수
@@ -26,3 +34,22 @@ console.log(arrowFun(5,6));
 type arrow = (x:number,y:number) => number
 const arrowFun2: arrow = (x,y) => x+y;
 console.log(arrowFun2(5,6));
+
+/**
+ * 배열
+ */
+const stringArray: string[] = ['1','2','3'];
+console.log(stringArray);
+
+const numberArray: number[] = [1,2,3];
+console.log(numberArray);
+
+//Tuple은 고정된 배열을 의미합니다.
+const tuple: [number,string,string,number] = [3,'1','2',3];
+console.log(tuple);
+
+/**
+ * 객체
+ */
+const object: {data:number,test:string} = {data:1,test:'test'};
+console.log(object);
